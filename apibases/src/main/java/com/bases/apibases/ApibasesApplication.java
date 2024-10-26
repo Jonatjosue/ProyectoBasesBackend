e39bd256750957883a6559bea56d3eb1e55d8455
Jonathan_C <Jonathan.Cifuentes2030@gmail.com>
2024-10-25 21:10:37 -0600

    proyecto backend bases

---
+++ apibases/src/main/java/com/bases/apibases/ApibasesApplication.java
@@ -0,0 +1,13 @@
+package com.bases.apibases;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApibasesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApibasesApplication.class, args);
+	}
+
+}
---
+++ apibases/src/test/java/com/bases/apibases/ApibasesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bases.apibases;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApibasesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
